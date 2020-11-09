457f9fe8d8db89f849833f6b56413d95ae5eb7ef
ttuseong <zlsp21128@naver.com>
2020-11-09 14:48:55 +0900

    땅따먹기 풀이 추가

---
+++ Programmers/Code/땅따먹기/땅따먹기.js
@@ -0,0 +1,14 @@
+function solution(land) {
+    var answer = 0;
+  
+    for(var i = 1; i < land.length; i++){
+        land[i][0] = Math.max(land[i-1][1], land[i-1][2], land[i-1][3])+land[i][0];
+        land[i][1] = Math.max(land[i-1][0], land[i-1][2], land[i-1][3])+land[i][1];
+        land[i][2] = Math.max(land[i-1][0], land[i-1][1], land[i-1][3])+land[i][2];
+        land[i][3] = Math.max(land[i-1][0], land[i-1][1], land[i-1][2])+land[i][3];
+    }
+    
+    answer = Math.max.apply(null, land[land.length-1]);
+
+    return answer;
+}
