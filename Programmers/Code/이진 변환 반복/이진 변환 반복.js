4c128201f019b6dbf333780e1047da85db11dd04
ttuseong <zlsp21128@naver.com>
2022-08-03 10:34:37 +0900

    이진 변환  반복

---
+++ Programmers/Code/이진 변환 반복/이진 변환 반복.js
@@ -0,0 +1,12 @@
+function solution(s) {
+    var answer = [0, 0];
+    
+    while(s != '1'){
+        answer[1] += s.match(/0/g) != null ? s.match(/0/g).length : 0 
+        answer[0]++;
+        s = s.replace(/0/g, "").length.toString(2);
+    }
+    
+    return answer;
+}
+
